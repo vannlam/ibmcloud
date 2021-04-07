8420f51f4a4762c6fe454dc70896878514c68862
vannlam <vann.lam@fr.ibm.com>
2021-04-07 21:32:08 +0200

    satellite script for VSI host

---
+++ satellite/add_rh_subscription_in_VSI.sh
@@ -0,0 +1,7 @@
+# https://cloud.ibm.com/docs/satellite?topic=satellite-ibm
+# test satellite with VSI in IBM cloud
+
+# add these 2 lines in your attach host script
+
+subscription-manager refresh
+subscription-manager repos --enable=*
