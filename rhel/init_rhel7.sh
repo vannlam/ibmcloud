502f89ccd66612567633fe4aad92cf33460eb603
vannlam <vann.lam@fr.ibm.com>
2021-04-09 23:41:15 +0200

    rhel7 init

---
+++ rhel/init_rhel7.sh
@@ -0,0 +1,37 @@
+
+
+# init rhel VSI
+
+# change password
+passwd
+
+# change sshd port
+sed -i "s:#Port.*:Port 32022:" /etc/ssh/sshd_config
+
+# Disable firewall
+systemctl stop firewalld
+systemctl disable firewalld
+
+# Disable SELINUX
+sed -i "s:SELINUX=.*:SELINUX=disable:" /etc/selinux/config
+setenforce 0
+systemctl restart sshd
+
+reboot
+
+# Do not forget to add inbound rule for port 32022 in VSI's Security Group
+
+
+
+# install Gnome
+yum -y update
+yum -y groupinstall "Server with GUI"
+
+# install VNC
+yum -y install tigervnc-server xorg-x11-fonts-Type1
+vncpasswd
+
+sed -i "s:# geometry:geometry:" .vnc/config
+vncserver
+# open tunnel for VNC
+ssh -p 32022 -fL 9901:localhost:5901 root@158.176.179.103 tail -f /dev/null
