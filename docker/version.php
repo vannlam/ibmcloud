921eaeeb743dc86b2ba61245642d64ed0c9cb435
vannlam <vann.lam@fr.ibm.com>
2025-01-14 14:31:16 +0100

    mount

--- docker/version.php
+++ docker/version.php
@@ -2,18 +2,32 @@
 require '/aws/aws-autoloader.php';
 use Aws\S3\S3Client;
 use Aws\Exception\AwsException;
+
+$env = parse_ini_file('/aws/s3-access');
+$region = $env["CE_S3_REGION"];
+$version = $env["CE_S3_VERSION"];
+$key = $env["CE_S3_KEY"];
+$secret = $env["CE_S3_SECRET"];
+$bucket = $env["CE_S3_BUCKET"];
+
+echo "CE_S3_REGION".$region;
+echo "CE_S3_VERSION".$version;
+echo "CE_S3_KEY".$key;
+echo "CE_S3_SECRET".$secret;
+echo "CE_S3_BUCKET".$bucket;
+
 $config = [
-                 'region' => 'eu-west-3',
-                 'version' => 'latest',
+                 'region' => $region,
+                 'version' => $version,
                  "Effect" => "Allow",
                  'credentials' => [
-                   'key' => $_ENV["CE_S3_KEY"],
-                   'secret' => $_ENV["CE_S3_SECRET"]
+                   'key' => $key,
+                   'secret' => $secret
                   ]
                 ];
 $s3client = new Aws\S3\S3Client($config);
 $file = $s3client->getObject([
-                         'Bucket' => 'bao3lei3',
+                         'Bucket' => $bucket,
                          'Key' => 'create_txt_record.json'
                        ]);
 $body = $file->get('Body');
